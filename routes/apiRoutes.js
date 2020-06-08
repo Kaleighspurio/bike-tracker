4a4073050a68eb8719bd8cf1da7c956f8f899d5a
Kaleigh Spurio <kaleighastell@gmail.com>
2020-06-08 19:30:48 -0400

    added api and html routes with basic server

---
+++ routes/apiRoutes.js
@@ -0,0 +1,7 @@
+// This creates a router so we can add our routes
+const router = require('express').Router();
+
+// add our routes
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes =require('./routes/htmlRoutes');
+
+
+const PORT = 8080;
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+})
