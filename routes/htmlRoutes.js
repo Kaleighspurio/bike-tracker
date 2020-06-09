dc5af06eb37af6db2050dc20877bba392a7f5d22
Kaleigh Spurio <kaleighastell@gmail.com>
2020-06-08 20:09:36 -0400

    added some basic html structure and html homepage route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,14 @@
 // This creates a router so we can add our routes
 const router = require('express').Router();
+const path = require('path');
 
 // add our routes
 
 // TODO: html route for displaying the home page
+router.get('/', (req, res) => {
+    const filePath = path.join(__dirname, '..', 'public', 'html', 'index.html');
+    res.sendFile(filePath);
+});
 
 
 module.exports = router;
