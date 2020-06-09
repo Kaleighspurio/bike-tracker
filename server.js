6990e7ed46db1e70d315d225a9ccce5cb2e0dcf3
Kaleigh Spurio <kaleighastell@gmail.com>
2020-06-08 21:18:50 -0400

    working graphs

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes =require('./routes/htmlRoutes');
 
+// This middleware that is needed to use POST data. It needs to go before the other app.use
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 const PORT = 8080;
 
